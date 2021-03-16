839902d3531811502fb8bdc9f9f73eaa8322fc64
Mohamed Saad <mohamedsaadamin.cis@gmail.com>
2021-03-16 22:44:48 +0200

    Interact with door

--- Assets/_RDMAAA/Scripts/LabDoor.cs
+++ Assets/_RDMAAA/Scripts/LabDoor.cs
@@ -1,17 +1,17 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody), typeof(SphereCollider))]
 public class LabDoor : InteractableObject
 {
     public new void Interact()
     {
         Debug.Log("door interacted");
-        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(0, 95, 0)), Time.deltaTime * 3f);
-        transform.localRotation = Quaternion.LookRotation(new Vector3(0, 95, 0));
+        //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(0, 95, 0), transform.up), Time.deltaTime * 3f);
+        transform.localRotation = Quaternion.LookRotation(new Vector3(95, 0, 0));
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Player"))
         {
             Interact();
